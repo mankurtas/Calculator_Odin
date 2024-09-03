aad64b19e1993429b37ef6e6c4229544c199c888
mankurtas <71133480+mankurtas@users.noreply.github.com>
2024-09-03 22:23:49 +0300

    Add styling to indicate which operator selected. Stop select firsst number after operator selected

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const clear = document.querySelector('.clear')
 const sign = document.querySelector('.sign')
 const percent = document.getElementsByClassName('.percent')
 
-const operators = document.getElementsByClassName('.operator')
+const operators = document.querySelectorAll('.operator')
 const operands = document.querySelectorAll('.operand')
 
 const zero = document.getElementById('zero')
@@ -18,16 +18,19 @@ const equals = document.getElementsByClassName('.equals')
 
 
 let firstNumber = Array (1).fill(0);
-let secondNumber = 0;
-let operator = '';
-let positiveNegative = '+'
+let secondNumber = Array (1).fill(0);
+let operator = Array (1).fill(0);
+let rezult =[];
 
 //Add listener to clear all
 
 clear.addEventListener('click', ()=> {
     firstNumber = [0];
     secondNumber = 0;
-    displayNumber()
+
+    operands.forEach(btn => btn.addEventListener('click', setFirstNumber))
+    operators.forEach(btn => btn.classList.remove('clicked'))
+    displayOutput()
 
 })
 
@@ -47,7 +50,7 @@ sign.addEventListener('click', () =>{
         firstNumber = numberString.split('');
     }
         
-    displayNumber()
+    displayOutput()
     
 })
 
@@ -61,7 +64,7 @@ function setDecimal(event) {
         console.log(firstNumber)
     }
     
-    displayNumber()
+    displayOutput()
 }
 // Listen Decimal
 decimal.addEventListener('click', setDecimal)
@@ -72,30 +75,57 @@ operands.forEach(element => {
     element.addEventListener ('click', setFirstNumber);
 });
 
+//Set First number
+
 function setFirstNumber(event){
 
    if(firstNumber.length >5){
         firstNumber = firstNumber
-    displayNumber()
+    displayOutput()
 
    } else if (firstNumber[0] == 0 && firstNumber.length == 1){
     firstNumber.fill(event.target.value)
     console.log(firstNumber)
-    displayNumber()
+    displayOutput()
 
    } else {
     firstNumber.push(event.target.value)
     console.log(firstNumber)
-    displayNumber()
+    displayOutput()
 
    }
 }
 
-function displayNumber (){
+//Function get operator
+
+let setOperator = function operatorSet (event){
+    operator.fill(event.target.value)
+    operators.forEach(btn => btn.classList.remove('clicked'))
+    event.target.classList.toggle('clicked')
+
+    operands.forEach(btn => btn.removeEventListener('click', setFirstNumber))
+
+}
+
+//Listen operators
+
+operators.forEach(element => {
+    element.addEventListener ('click', setOperator);
+});
+
+
+
+//Display Output
+
+function displayOutput (){
     display.textContent = firstNumber.join('').toString();
     console.log(`Lenght: ${firstNumber.length}`)
     console.log(`Type of: ${typeof(firstNumber)}`)
     console.log(`First number: ${firstNumber}`)
+    console.log(`Operator ${operator}`)
+    // console.log(`Lenght: ${secondNumber.length}`)
+    // console.log(`Type of: ${typeof(secondNumber)}`)
+    // console.log(`First number: ${secondNumber}`)
 }
 
-displayNumber()
+displayOutput()
